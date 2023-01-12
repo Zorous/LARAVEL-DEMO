c8576cf66e5f2f96204fde9196802cf01d704e0a
Zorous <oussamabelhadi5@gmail.com>
2023-01-12 23:50:59 +0100

    init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+
+@section('title',"about")
+
+@section('content')
+<h1>Welcome</h1>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+
+@section('title',"Welcome")
+
+@section('content')
+<h1>Welcome</h1>
+@endsection
